e2b84841754f2ff6947305e3492b45956e1fc05a
saphiatoutoure765-jpg <saphiatoutoure765@icloud.com>
2026-03-13 12:59:57 +0000

    feat: display projects list using ProjectProvider

--- lib/screens/home/tabs/projects_tab.dart
+++ lib/screens/home/tabs/projects_tab.dart
@@ -1,39 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:sunu_task/core/constants/app_colors.dart';
+import 'package:provider/provider.dart';
 
-// === PARTIE 4.3 ===
-// Liste des projets
+import '../../../providers/project_provider.dart';
+import '../../../widgets/cards/project_card.dart';
+import '../../../core/constants/app_colors.dart';
+import '../../projects/project_detail_screen.dart';
 
 class ProjectsTab extends StatelessWidget {
   const ProjectsTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
 
-          Icon(
-            Icons.folder_open,
-            size: 80,
-            color: AppColors.textSecondary,
-          ),
+    final projectProvider = context.watch<ProjectProvider>();
+    final projects = projectProvider.projects;
 
-          const SizedBox(height: 20),
+    /// si aucun projet
+    if (projects.isEmpty) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
 
-          const Text(
-            "Aucun projet pour le moment",
-            style: TextStyle(fontSize: 18),
-          ),
+            Icon(
+              Icons.folder_open,
+              size: 80,
+              color: AppColors.textSecondary,
+            ),
 
-          const SizedBox(height: 10),
+            const SizedBox(height: 20),
 
-          const Text(
-            "Cliquez sur + pour créer un projet",
-          ),
-        ],
-      ),
+            const Text(
+              "Aucun projet pour le moment",
+              style: TextStyle(fontSize: 18),
+            ),
+
+            const SizedBox(height: 10),
+
+            const Text(
+              "Cliquez sur + pour créer un projet",
+            ),
+          ],
+        ),
+      );
+    }
+
+    /// si projets existent
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: projects.length,
+      itemBuilder: (context, index) {
+
+        final project = projects[index];
+
+        return ProjectCard(
+          project: project,
+          taskCount: 0,
+
+          onTap: () {
+
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => ProjectDetailScreen(
+                  project: project,
+                ),
+              ),
+            );
+
+          },
+        );
+      },
     );
   }
 }
